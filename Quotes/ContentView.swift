623fb167b8a6d491280c4346dfa9589617f91298
abhi4gitt <raiabhishek631@gmail.com>
2026-01-02 20:30:40 +0530

    Initial Commit

---
+++ Quotes/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Quotes
+//
+//  Created by Abhishek on 02/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Tab("Breaking Bad", systemImage: "tortoise") {
+                Text("Breaking Bad View")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+            }
+            
+            Tab("Better Call Saul", systemImage: "briefcase") {
+                Text("Better Call Saul View")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+            }
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Abhishek on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
