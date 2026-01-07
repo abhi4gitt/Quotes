bf7a59da746fdee82da92ed0f2544a757b4f969a
abhi4gitt <raiabhishek631@gmail.com>
2026-01-07 22:47:37 +0530

    Complete fetch quote function to get quote

--- Quotes/FetchService.swift
+++ Quotes/FetchService.swift
@@ -14,12 +14,26 @@ struct FetchService {
     
     let baseURL = URL(string: "https://breaking-bad-api-six.vercel.app/api")!
     
-    //FetchURL : https://breaking-bad-api-six.vercel.app/api/quotes/random?production=Breaking+Bad
+    // FetchURL : https://breaking-bad-api-six.vercel.app/api/quotes/random?production=Breaking+Bad
     
     // Build fetch url
     func fetchQuote(from show: String) async throws -> Quote {
         
         let quoteURL = baseURL.appending(path: "quotes/random")
         let fetchURL = quoteURL.appending(queryItems: [URLQueryItem(name: "production", value: show)])
+        
+        // Fetch data
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        // Handle response
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        // Decode data
+        let quote = try JSONDecoder().decode(Quote.self, from: data)
+        
+        // Return quote
+        return quote
     }
 }
