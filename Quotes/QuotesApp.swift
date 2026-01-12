bb2240b07503a0f68c8e12246a767f838bf28df8
abhi4gitt <raiabhishek631@gmail.com>
2026-01-12 08:39:04 +0530

    Add El Camino tab. Utilize all character images and CharcterView

--- Quotes/QuotesApp.swift
+++ Quotes/QuotesApp.swift
@@ -15,3 +15,13 @@ struct QuotesApp: App {
         }
     }
 }
+
+/*
+ Version 2 Feature List
+ ✅ Add El Camino tab
+ ✅ Utilize all character images and CharcterView
+ - On CharacterView, auto-scroll to bottom after status is shown
+ - Fetch episode data
+ - Extend string to get rid of long images and color names
+ - Create static constants for show names
+ */
